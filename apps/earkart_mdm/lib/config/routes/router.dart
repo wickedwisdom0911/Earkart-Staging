85cb0e74eaccd01755b08e6f78060146b968b32a
Deev Saini <divyanshsaini13@gmail.com>
2025-06-02 14:21:40 +0530

    chore(router): comment out unused arguments in route generation for cleaner code

--- apps/earkart_mdm/lib/config/routes/router.dart
+++ apps/earkart_mdm/lib/config/routes/router.dart
@@ -9,7 +9,7 @@ import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   Logger l = Logger();
-  final args = settings.arguments;
+  // final args = settings.arguments;
   l.i(settings.name);
   Route<dynamic> router(Widget w) {
     return MaterialWithModalsPageRoute(builder: (_) => w, settings: settings);
