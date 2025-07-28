0ddc1653c02cde5833e31f3217a9c8bc69646f59
wickedwisdom0911 <shivamrawat091125@gmail.com>
2025-07-28 14:00:03 +0530

    video flipped final

--- apps/earkart_omni/lib/features/consultation/presentation/widgets/video_call_widget.dart
+++ apps/earkart_omni/lib/features/consultation/presentation/widgets/video_call_widget.dart
@@ -161,7 +161,7 @@ class _VideoCallWidgetState extends State<VideoCallWidget>
           dimensions: VideoDimensions(width: 1280, height: 720),
           frameRate: 30,
           bitrate: 2500,
-          mirrorMode: VideoMirrorModeType.videoMirrorModeDisabled, // Disable mirroring to fix orientation
+          mirrorMode: VideoMirrorModeType.videoMirrorModeAuto,
           minBitrate: 1000,
           degradationPreference: DegradationPreference.maintainQuality,
         ),
